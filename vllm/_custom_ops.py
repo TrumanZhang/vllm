d20378c1cbe5ee2cd14379d1b8ee68749b1db848
wbdr <1603796585@qq.com>
2024-08-15 10:26:13 +0800

    fix runtime bugs-25

--- vllm/_custom_ops.py
+++ vllm/_custom_ops.py
@@ -143,6 +143,39 @@ def paged_attention_v2(
         blocksparse_block_size, blocksparse_head_sliding_step)
 
 
+def paged_attention_v3(
+    out: torch.Tensor,
+    out_exp_sum: torch.Tensor,
+    out_max_logits: torch.Tensor,
+    exp_sum: torch.Tensor,
+    max_logits: torch.Tensor,
+    tmp_out: torch.Tensor,
+    query: torch.Tensor,
+    key_cache: torch.Tensor,
+    value_cache: torch.Tensor,
+    num_kv_heads: int,
+    scale: float,
+    block_tables: torch.Tensor,
+    seq_lens: torch.Tensor,
+    block_size: int,
+    max_seq_len: int,
+    alibi_slopes: Optional[torch.Tensor],
+    kv_cache_dtype: str,
+    kv_scale: float,
+    tp_rank: int = 0,
+    blocksparse_local_blocks: int = 0,
+    blocksparse_vert_stride: int = 0,
+    blocksparse_block_size: int = 64,
+    blocksparse_head_sliding_step: int = 0,
+) -> None:
+    torch.ops._C.paged_attention_v3(
+        out, out_exp_sum, out_max_logits, exp_sum, max_logits, tmp_out, query,
+        key_cache, value_cache, num_kv_heads, scale, block_tables, seq_lens,
+        block_size, max_seq_len, alibi_slopes, kv_cache_dtype, kv_scale,
+        tp_rank, blocksparse_local_blocks, blocksparse_vert_stride,
+        blocksparse_block_size, blocksparse_head_sliding_step)
+
+
 def paged_attention_remote(
     out: torch.Tensor,
     out_exp_sum: torch.Tensor,
