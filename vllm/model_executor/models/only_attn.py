095456267b61be86edc73d2e91a0606e369f1f78
wbdr <1603796585@qq.com>
2024-09-11 15:13:03 +0800

    fix sequence-parallel-new bug-20

--- vllm/model_executor/models/only_attn.py
+++ vllm/model_executor/models/only_attn.py
@@ -99,14 +99,16 @@ class OnlyAttention(nn.Module):
         kv_cache: torch.Tensor,
         attn_metadata: AttentionMetadata,
     ) -> None:
-        q=torch.empty([attn_metadata.num_long_decode_tokens,self.num_heads,self.head_dim],device='cuda')
-        gather=self.broastcastlayer.forward(q)
-        length=gather.size(0)
-        q,_ = gather.split([self.tp_size,length-self.tp_size],dim=0)
-        attn_to_reduce, exp_sum_to_reduce, max_logits_to_reduce = self.attn(
-            q, kv_cache, attn_metadata, self.sp_rank)
-        self.gatherlayer.forward(attn_to_reduce[0], exp_sum_to_reduce[0],
-                         max_logits_to_reduce[0])
+        if attn_metadata.num_long_decode_tokens>0:
+            q=torch.empty([attn_metadata.num_long_decode_tokens,self.num_heads,self.head_dim],device='cuda')
+            
+            gather=self.broastcastlayer.forward(q)
+            length=gather.size(0)
+            q,_ = gather.split([self.tp_size,length-self.tp_size],dim=0)
+            attn_to_reduce, exp_sum_to_reduce, max_logits_to_reduce = self.attn(
+                q, kv_cache, attn_metadata, self.sp_rank)
+            self.gatherlayer.forward(attn_to_reduce[0], exp_sum_to_reduce[0],
+                            max_logits_to_reduce[0])
 
 
 class OnlyAttentionLayer(nn.Module):
