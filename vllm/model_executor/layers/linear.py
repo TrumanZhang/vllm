4fbecf1fac66ef23df312d29443a0a7bc777e914
wbdr <1603796585@qq.com>
2024-09-12 21:02:51 +0800

    fix new-sequence-parallel bug-36

--- vllm/model_executor/layers/linear.py
+++ vllm/model_executor/layers/linear.py
@@ -703,7 +703,9 @@ class SequenceParallelLinearForGather:
         # gather(input_,dst,dim),dim is untest.
         # output need be the shape
         # [num_seqs, num_heads, num_sequece_block, head_size]
-        
+        size=list(input_.size())
+        str=",".join(size)
+        logger.info("input_size=%s,global_rank=%d",str,get_sp_group(0).rank_in_group)
         output = get_sp_group(self.tp_rank).all_gather_extension(input_, -1)
         output2 = get_sp_group(self.tp_rank).all_gather_extension(input_2, -1)
         output3 = get_sp_group(self.tp_rank).all_gather_extension(input_3, -1)
