128f7f3b3cb0dcdc93272abc3c5068883761d21f
dhy <5130061+dhyyhddhy@user.noreply.gitee.com>
2024-09-16 20:56:11 +0800

    fix new-sequence-parallel bug-86

--- vllm/distributed/parallel_state.py
+++ vllm/distributed/parallel_state.py
@@ -454,9 +454,9 @@ class GroupCoordinator:
         # Bypass the function if we are using only 1 GPU.
         if self.world_size == 1:
             return obj
-        if self.shm_broadcaster is not None:
-            assert src == 0, "Shared memory broadcaster only supports src=0"
-            return self.shm_broadcaster.broadcast_object(obj)
+        # if self.shm_broadcaster is not None:
+        #     assert src == 0, "Shared memory broadcaster only supports src=0"
+        #     return self.shm_broadcaster.broadcast_object(obj)
         if self.rank_in_group == src:
             torch.distributed.broadcast_object_list([obj],
                                                     src=self.ranks[src],
